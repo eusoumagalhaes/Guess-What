4cd9c4df1fa34eb3d5f7be6e52aa8d759999dac5
Vinicius Magalhaes de Oliveira <vinicius.oliveira@matriz.fornecedoramaquinas.com.br>
2022-08-11 14:18:10 -0300

    adding the home page

---
+++ src/Components/home.js
@@ -0,0 +1,13 @@
+import './home.css'
+
+const home = () => {
+  return (
+    <div className='home'>
+        <h1>Guess The Marvel</h1>
+        <p>Click on the button bellow and start the guessing</p>
+        <button className='btn'>BEGIN</button>
+    </div>
+  )
+}
+
+export default home
---
+++ src/Data/words.js
@@ -0,0 +1,6 @@
+export const wordsList = {
+    avengers: ["Captain America", "Iron Man", "Thor", "Hulk", "Black Window", "Scalert Witch"],
+    spiderman: ["Spider-Man", "Green Goblin", "Miles Morales", "Doc Ock", "Electro", "Peter Parker"],
+    fantasticfour: ["Mr.Fantastic", "Invisible Woman", "Human Torch", "The Thing", "Dr.Doom"],
+    xmen: ["Wolverine", "Cyclops", "Storm", "Beast", "Magneto", "Professor X"]
+};
